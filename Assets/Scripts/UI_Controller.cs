01cabe420696878f06a0c1f27101338cd534c9f6
Mikołaj czarnecki <wertynius2@gmail.com>
2024-01-27 13:03:36 +0000

    Kitchen - UI - reload ingredients from

--- Assets/Scripts/UI_Controller.cs
+++ Assets/Scripts/UI_Controller.cs
@@ -7,8 +7,11 @@ public class UI_Controller : MonoBehaviour
 {
     public Animator Anim;
     public Transform canvas;
-    public Transform ScreenOrder, ScreenKitchen, ScreenMenu; //Different gamescreens;
+    public Transform ScrollableParent; //Different gamescreens;
     public static UI_Controller instance;
+
+    [SerializeField] GameObject[] NewOptions = new GameObject[0];
+    //public 
 
     public void Awake()
     {
@@ -16,5 +19,29 @@ public class UI_Controller : MonoBehaviour
     }
 
     public void PlayAnim(string clip) { Debug.Log("Playing animation "+clip); Anim.SetTrigger(clip); }
+
+    public void OnDrawerOpen(string category) 
+    {
+        PlayAnim("Drawer");
+
+        //Remove the Prefabs already in the scene
+        foreach (var D in ScrollableParent.transform.GetComponentsInChildren<Draggable>()) 
+        {
+            Destroy(D.transform.parent);
+        }
+
+        //fill with RELEVANT prefabs
+        NewOptions = Resources.LoadAll("Ingredient Objects") as GameObject[];
+        foreach (var item in NewOptions)
+        {
+            //if(item.GetComponentInChildren< Draggable>().category !=category) {continue; }
+
+            GameObject Ingredient = Instantiate(Resources.Load("Ingredient") as GameObject, ScrollableParent);
+            if(Ingredient.TryGetComponent(out Draggable D)) 
+            {
+            //TODO replace blank data with the relevant data here!
+            }
+        }
+    }
 
 }
